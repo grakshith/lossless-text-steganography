6cae0bd03ac99a04d54d691a54f9b09ba4680688
Rakshith <hehaichi@gmail.com>
2018-02-13 12:38:32 +0530

    Refactors the code

--- huffman_encode.py
+++ huffman_encode.py
@@ -1,60 +1,53 @@
 import huffman
 
+class SecretMessage:
+	def __init__(self, message):
+		self.codewords = {}
+		self.d_codewords = {}
+		self.message = message
 
-codewords = {}
-d_codewords = {}
+	def string_to_binary(self):
+		return ''.join(format(ord(c), 'b') for c in self.message)
 
-def string_to_binary(string):
-	return ''.join(format(ord(x), 'b') for x in string)
+	def make_symbol_list(self, bin_str, n):
+		self.symbol_list = [bin_str[i:i+n] for i in range(0, len(bin_str), n)]
+		return self.symbol_list
 
-# print string_to_binary("A")
+	def encode(self):
+		binary = self.string_to_binary()
+		self.symbol_list = self.make_symbol_list(binary, 4)
+		freq_table = {}
+		for symbol in self.symbol_list:
+			if freq_table.get(symbol):
+				freq_table[symbol] = freq_table[symbol]+1
+			else:
+				freq_table[symbol] = 1
+		symbol_tup_list = []
+		for symbol in freq_table:
+			symbol_tup_list.append((symbol,freq_table[symbol]))
 
-def make_list(bin_str, n):
-	symbol_list = []
-	symbol_list = [bin_str[i:i+n] for i in range(0, len(bin_str), n)]
-	return symbol_list
+		self.codewords = huffman.codebook(symbol_tup_list)
+		self.d_codewords = {self.codewords[key]:key for key in self.codewords}
+		self.message = ''.join(self.codewords[symbol] for symbol in self.symbol_list)
+		return self.message
+
+	def decode(self):
+		i=0
+		j=0
+		decoded = []
+		while i < len(self.message):
+			if self.d_codewords.get(self.message[i:i+j]):
+				decoded.append(self.d_codewords.get(self.message[i:i+j]))
+				i=i+j
+				j=0
+			else:
+				j = j+1
+		self.message = ''.join(decoded)
+		return self.message
 
 
-def encode(text):
-	binary = string_to_binary(text)
-	symbol_list = make_list(binary, 4)
-	print symbol_list
-	freq_table = {}
-	for symbol in symbol_list:
-		if freq_table.get(symbol):
-			freq_table[symbol] = freq_table[symbol]+1
-
-		else:
-			freq_table[symbol] = 1
-	symbol_tup_list = []
-	for symbol in freq_table:
-		symbol_tup_list.append((symbol,freq_table[symbol]))
-
-	print symbol_tup_list
-	# global codewords, d_codewords
-	codewords = huffman.codebook(symbol_tup_list)
-	print codewords
-	d_codewords = {codewords[key]:key for key in codewords}
-	# print ''.join(symbol for symbol in symbol_list)
-	# print ''.join(codewords[symbol] for symbol in symbol_list)
-	return ''.join(codewords[symbol] for symbol in symbol_list)
-
-encode("AAA")
-# print make_list("101000111", 4)
-# print huffman.codebook([('A', 2), ('B', 4), ('C', 1), ('D', 1)])	
-
-print codewords
-def decode(text):
-	i=0
-	j=0
-	decoded = []
-	while i < len(text):
-		if d_codewords.get(text[i:i+j]):
-			decoded.append(d_codewords.get(text[i:i+j]))
-			i=i+j
-			j=0
-		else:
-			j = j+1
-	print ''.join(decoded)
-
-decode('10000111011101')
+if __name__ == '__main__':
+	obj = SecretMessage("AAA")
+	print obj.string_to_binary()
+	print obj.encode()
+	print obj.decode()
