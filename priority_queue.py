4c162716742cd35ad3b4842adc0ad470d45a24b1
Rakshith <hehaichi@gmail.com>
2018-02-13 18:29:05 +0530

    Modifies priority queue implementation to work with TreeNode

--- priority_queue.py
+++ priority_queue.py
@@ -1,4 +1,9 @@
-
+class TreeNode:
+    def __init__(self, data):
+        self.parent = None
+        self.left = None
+        self.right = None
+        self.data = data
 
 class PriorityQueue:
 
@@ -6,15 +11,15 @@ class PriorityQueue:
 
 		self.heap = array
 		self.heap_size = len(array)
-	
+
 	def get_minimum(self):
 
 		return self.heap[0]
 
 	def extract_minimum(self):
-		if self.heap_size < 0:
+		if self.heap_size <= 0:
 			return -1
-		
+
 		heap_min = self.heap[0]
 		self.heap[0] = self.heap[self.heap_size-1]
 		self.heap_size = self.heap_size-1
@@ -24,12 +29,12 @@ class PriorityQueue:
 	def min_heapify(self, i):
 		l = 2*i+1
 		r = 2*i+2
-		if l<self.heap_size and self.heap[l] < self.heap[i]:
+		if l<self.heap_size and self.heap[l].data[0] < self.heap[i].data[0]:
 			largest = l
 		else:
 			largest = i
 
-		if r<self.heap_size and self.heap[r] < self.heap[largest]:
+		if r<self.heap_size and self.heap[r].data[0] < self.heap[largest].data[0]:
 			largest = r
 
 		if largest != i:
@@ -43,15 +48,17 @@ class PriorityQueue:
 			print i
 			self.min_heapify( i)
 
+	def get_heap(self):
+		return [x.data for x in self.heap[0:self.heap_size]]
 
 	def insert_min_heap(self, key):
 		self.heap_size = self.heap_size+1
 		if(len(self.heap) == self.heap_size-1):
 			self.heap.append(key)
-		else:	
+		else:
 			self.heap[self.heap_size-1] = key
 		i = self.heap_size-1;
-		while i>=1 and self.heap[(i+1)/2-1] > self.heap[i]:
+		while i>=1 and self.heap[(i+1)/2-1].data[0] > self.heap[i].data[0]:
 			temp = self.heap[i]
 			self.heap[i] = self.heap[(i+1)/2-1]
 			self.heap[(i+1)/2-1] = temp
@@ -60,17 +67,17 @@ class PriorityQueue:
 
 
 if __name__ == '__main__':
-	obj = PriorityQueue([8, 9, 12, 10, 13, 15, 17])
+	obj = PriorityQueue([TreeNode((8,'0110')), TreeNode((9,'1111')), TreeNode((12,'0000')),
+						TreeNode((10,'1101')), TreeNode((13,'1100')), TreeNode((15,'1011')),
+						TreeNode((17,'1111'))])
 	obj.build_min_heap()
 	# obj.insert_min_heap(6)
-	print obj.heap
-	print obj.extract_minimum()
-	print obj.heap
-	print obj.extract_minimum()
-	print obj.heap
-	print obj.extract_minimum()
-	print obj.heap
-	obj.insert_min_heap(6)
-	print obj.heap	
-	
-	
+	print obj.get_heap()
+	print obj.extract_minimum().data
+	print obj.get_heap()
+	print obj.extract_minimum().data
+	print obj.get_heap()
+	print obj.extract_minimum().data
+	print obj.get_heap()
+	obj.insert_min_heap(TreeNode((6,'1110')))
+	print obj.get_heap()
