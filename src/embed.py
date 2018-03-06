3764cb920fc541c2c3745e8b74ab6e21d2774ea6
Suhas <suhasbs.nitk@gmail.com>
2018-03-07 00:50:47 +0530

    De-embedding done

--- src/embed.py
+++ src/embed.py
@@ -29,11 +29,16 @@ def embed(indices, message, file_string):
     message = list(message)
     file_string = list(file_string)
     with io.open('embedded/stego.txt', 'w', encoding='utf-8') as file:
-        for index in indices:
-            if(message[0]=="1"):
-                file_string[index] = u'\u2063'.encode('utf-8')
+        for index,m in zip(indices,message):
+            print index
+            print m
+            if(m=="1"):
+                file_string[index] = u'\u2008'.encode('utf-8')
+            
+            
         # file.write(''.join(file_string).encode('ascii'))
         # file_string = ''.join(file_string)
+        print file_string
         s=u''
         for x in file_string:
             # print type(x.decode('utf-8'))
@@ -46,15 +51,46 @@ def embed(indices, message, file_string):
     pdf.multi_cell(h=5.0, align='L', w=0, txt=s, border=0)
     pdf.output('embedded/pdf.pdf', 'F')
 
+
+
+def de_embed(filename, seed, length):
+    with io.open(filename, 'r', encoding='utf-8') as file:
+    # with open(filename) as file:
+        i=0
+        message_dict = {}
+        empty_chars = []
+        
+        for line in file:
+            for char in line:
+                
+                if char == u'\u2008':
+                    message_dict[i] = 1 
+                    empty_chars.append(i)
+                if char == ' ':
+                    message_dict[i] = 0
+                    empty_chars.append(i)
+                    
+                i+=1    
+    
+    random.seed(seed)
+    random.shuffle(empty_chars)
+    decoded_message = ''
+    for i in range(0, length):
+        decoded_message += str(message_dict[empty_chars[i]])
+
+    print decoded_message 
+    return decoded_message
+
 if __name__ == '__main__':
     if(len(sys.argv)) < 2:
         print "One argument is required"
         exit(0)
     message = "1010111101110001010000"
+    
     indices, file_string = get_spaces(str(sys.argv[1:2][0]))
     if len(indices)<len(message):
         print "Number of empty characters in the cover text is very small"
         exit(0)
     random_indices = randomize_indices(indices)
     embed(random_indices, message, file_string)
-
+    de_embed('embedded/stego.txt', 60 ,len(message))
